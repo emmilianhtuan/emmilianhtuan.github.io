c206669d83ac6fb68a1f2d49f5afea62361bff6d
emmilianhtuan <74419139+emmilianhtuan@users.noreply.github.com>
2021-01-11 19:22:35 +0700

    alo

---
+++ index.js
@@ -0,0 +1,156 @@
+// let data = [{
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/a/o/ao-dai-tay-so-sinh-kiza-in-hinh-chuot-th-0-3m-1.jpg",
+//         name: "Áo dài tay sơ sinh in hình chuột",
+//         price: 20000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/a/o/ao-dai-tay-so-sinh-kiza-in-hinh-huou-cao-co-0-3m-trang-vang-1.jpg",
+//         name: "Áo dài tay sơ sinh in hình huơu",
+//         price: 20000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/a/o/ao-so-sinh-dai-tay-kiza-in-hinh-ca-heo-0-3m-trang-xanh-1_1.jpg",
+//         name: "Áo dài tay sơ sinh in hình cá heo",
+//         price: 20000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/a/o/ao-so-sinh-dai-tay-kiza-in-su-tu-trang-cam-0-3m-1_1.jpg",
+//         name: "Áo dài tay sơ sinh in hình sư tử",
+//         price: 20000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/a/o/ao-so-sinh-kiza-coc-in-hinh-tho-trang-hong-0-3m-1_1_1_1_1_1.jpg",
+//         name: "Áo cộc tay sơ sinh in hình chú thỏ",
+//         price: 20000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/a/o/ao-coc-tay-so-sinh-kidsplaza-in-hinh-ca-heo-tx_1.jpg",
+//         name: "Áo cộc tay sơ sinh in hình cá heo",
+//         price: 20000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/q/u/quan-dai-so-sinh-kiza-in-hinh-chuot-mickey-trang-hong-0-3m-1.jpg",
+//         name: "Quần dài sơ sinh in hình huơu",
+//         price: 30000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/q/u/quan-dai-so-sinh-kiza-in-hinh-huou-cao-co-0-3m-trang-vang-1.jpg",
+//         name: "Áo cộc tay sơ sinh in hình cá heo",
+//         price: 30000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/300x/9df78eab33525d08d6e5fb8d27136e95/q/u/quan-dai-so-sinh-kiza-in-ca-heo-trang-xanh-0-3m-1_1.jpg",
+//         name: "Quần dài sơ sinh in hình cá heo",
+//         price: 30000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/q/u/quan-so-sinh-coc-kiza-in-hinh-ca-heo-trang-xanh-0-3m-1_1_1_2.jpg",
+//         name: "Quần cộc sơ sinh in hình cá heo",
+//         price: 30000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/q/u/quan-so-sinh-coc-kiza-in-hinh-chu-tho-trang-hong-0-3m-1_1_1_1.jpg",
+//         name: "Quần cộc sơ sinh in hình chú thỏ",
+//         price: 30000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/q/u/quan-so-sinh-coc-kiza-in-hinh-huou-cao-co-trang-vang-0-3m-1_1_1_1.jpg",
+//         name: "Quần cộc sơ sinh in hình hươu",
+//         price: 30000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-dai-tay-kidsplaza-neil-in-hinh-gau-0-3m-hn20t-trang-xanh-1_1.jpg",
+//         name: "Body dài tay hình gấu xanh- trắng",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-dai-tay-mamago-yem-meo-hn20t-xanh-0-3m-1.jpg",
+//         name: "Body dài tay in hình mèo",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-dai-tay-kidsplaza-neil-in-hinh-gau-0-3m-hn20t-trang-hong-1.jpg",
+//         name: "Body dài tay hình gấu hồng",
+//         price: 40000
+//     },
+    
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-dai-tay-bit-chan-kidsplaza-neil-gau-0-3m-hn20t-trang-xanh-1_1.jpg",
+//         name: "Body dài tay bít chân xanh-trắng",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-dai-tay-bit-chan-kidsplaza-neil-gau-0-3m-hn20t-trang-hong-1.jpg",
+//         name: "Body dài tay bít chân hồng",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-dai-tay-bit-chan-kidsplaza-neil-gau-0-3m-hn20t-vang-1_1.jpg",
+//         name: "Body dài tay bít chân vàng",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-so-sinh-dai-tay-day-hinh-ca-map-0-3m-kidsplaza-hn20d-be-ghi-1_2.jpg",
+//         name: "Body dài tay hình cá mập",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-so-sinh-dai-tay-day-ke-0-3m-kidsplaza-hn20d-xanh-1_1.jpg",
+//         name: "Body dài tay kẻ xanh",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-so-sinh-dai-tay-lien-chan-day-ke-kidsplaza-hn20d-0-3m-hong-1_1.jpg",
+//         name: "Body dài tay kẻ hồng",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-so-sinh-dai-tay-chan-day-0-3m-kidsplaza-hn20d-ghi-1.jpg",
+//         name: "Body dài tay kẻ trắng",
+//         price: 40000
+//     },
+//     {
+//         img: "https://kidsplaza-1.cdn.vccloud.vn/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/b/o/body-chan-day-vang-ke_0-3m-kidsplaza-hn20d-1_1.jpg",
+//         name: "Body dài tay kẻ vàng",
+//         price: 40000
+//     },
+// ]
+
+// let showIteam1 = (d) => {
+
+//     let dom = document.querySelector('.cai1')
+//     dom.innerHTML = ''
+//     for (let i of d) {
+//         let html = `<li class="splide__slide">
+//     <div class="card" style="width: 18rem;">
+//       <img class="card-img-top" src="${i.img}" alt="Card image cap">
+//       <div class="card-body">
+//         <h5 class="card-title">${i.name}</h5>
+//         <p class="card-text">${i.price}</p>
+//         <a href="file:///C:/Users/ADMIN/OneDrive/M%C3%A1y%20t%C3%ADnh/tuan/coding/C4EJS103/Day%209/index1.html" class="btn btn-primary">Xem chi tiết</a>
+//       </div>
+//     </li>`
+//         dom.innerHTML += html
+//     }
+// }
+
+// showIteam1(data)
+
+
+
+// let form = document.querySelector('#search-a')
+// form.onsubmit = (e) => {
+//     e.preventDefault()
+
+//     let inputSearch = form.search.value
+
+//     let result = data.filter(a => a.name.toLowerCase().includes(inputSearch.toLowerCase()))
+//     showIteam1(result)
+
+//     // .filter(country =>
+//     //     country.name.toLowerCase().includes(search_term.toLowerCase())
+//     // )
+//     // .forEach(country => {
+
+
+// }
